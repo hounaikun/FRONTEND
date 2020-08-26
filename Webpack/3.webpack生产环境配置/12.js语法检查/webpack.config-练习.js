c2b29cd8a1f9ef759cc29003fabc15d0aa2f1ef4
hounk <hounk@bijienetworks.com>
2020-08-26 16:46:22 +0800

    addcode

---
+++ Webpack/3.webpack生产环境配置/12.js语法检查/webpack.config-练习.js
@@ -0,0 +1,28 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_moudles/,
+        load: 'eslint-loader',
+        options: {
+          fix: true
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  mode: 'development'
+};
---
+++ Webpack/3.webpack生产环境配置/13.js兼容性处理/webpack.config练习.js
@@ -0,0 +1,32 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+              [
+                  '@babel/polyfill',
+              ]
+          ]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  mode: 'development'
+};
